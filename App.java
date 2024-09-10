ec9663415782070b67be77eccb8b76f11e5b2ef7
mikael21000 <77319736+Mikael21000@users.noreply.github.com>
2024-09-10 18:07:18 +0300

    Create App.java

---
+++ App.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner in = new Scanner(System.in);
+        String answer = "Emma";
+        String guess = "";
+        int guessCounter = 0;
+
+        do {
+            System.out.println("Guess my name (type stop to exit)");
+            guess = in.nextLine();
+            if (!guess.equals("stop")) { 
+                guessCounter++; 
+            }
+        } while (!answer.equals(guess) && !guess.equals("stop"));
+
+        if (answer.equals(guess)) {
+            System.out.println("Congratulations!");
+        }
+
+        System.out.println("You guessed " + guessCounter + " times.");
+
+    }
+    
+}
